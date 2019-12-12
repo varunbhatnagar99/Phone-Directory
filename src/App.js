82584433458a158415deadfcf34583d2f3fafdea
Varun Bhatnagar <varun41633@gmail.com>
2019-12-12 17:00:34 +0530

    Added delete button

--- src/App.js
+++ src/App.js
@@ -34,6 +34,10 @@ let subscribers=[
            return <div key={sub.id} className="grid-container">
              <span className="grid-item">{sub.name}</span>
              <span className="grid-item">{sub.phone}</span>
+             <span className="grid-item action-btn-container">
+               <button className="custom-btn delete-btn">Delete</button>
+               
+             </span>
              </div>
          })
        }
