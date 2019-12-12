20c172ced24dad87f0093f137b553e879b1ce65a
Varun Bhatnagar <varun41633@gmail.com>
2019-12-12 16:51:29 +0530

    Styled 'App' (Home Page)

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ render()
    <div>
     
      <Header />
-     <button>Add</button>
-     <div>
-       <span>Name</span><br />
-       <span>Phone</span>
+     <div className="component-body-container">
+       <button className="custom-btn add-btn">Add</button>
+       <div className="grid-container heading-container">
+         <span className="grid-item name-heading">Name</span>
+        <span className="grid-item phone-heading">Phone</span>
+       </div>
      </div>
    </div>
   );
