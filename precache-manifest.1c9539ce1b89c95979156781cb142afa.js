72ed0a187c6977eff863fd5d924f3ff01d480dac
Varun Bhatnagar <varun41633@gmail.com>
2019-12-12 23:25:04 +0530

    Updates

---
+++ precache-manifest.1c9539ce1b89c95979156781cb142afa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d4e0c8eb5e09b7a8d358d6f737ebc9",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "1464fe27853979e956bd",
+    "url": "/phone-directory/static/css/main.6f8ea97c.chunk.css"
+  },
+  {
+    "revision": "95bb3a15269695bfa7f2",
+    "url": "/phone-directory/static/js/2.cd341680.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/phone-directory/static/js/2.cd341680.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1464fe27853979e956bd",
+    "url": "/phone-directory/static/js/main.06065509.chunk.js"
+  },
+  {
+    "revision": "505cdc9333590f35c409",
+    "url": "/phone-directory/static/js/runtime-main.53d98e78.js"
+  }
+]);
